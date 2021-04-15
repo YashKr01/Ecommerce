24d5680de5423359ddea39e67678be54132fddd9
Mayank Bhatnagar <bhatnagar2212@gmail.com>
2021-04-15 18:03:08 +0530

    Refactored Fragments

--- app/src/main/java/com/shopping/bloom/fragment/CategoryFragment.java
+++ app/src/main/java/com/shopping/bloom/fragment/CategoryFragment.java
@@ -1,22 +1,22 @@
-package com.shopping.bloom.activities.fragments;
+package com.shopping.bloom.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.shopping.bloom.R;
 
+public class CategoryFragment extends Fragment {
 
-public class ProfileFragment extends Fragment {
-    private static final String TAG = ProfileFragment.class.getName();
+    private static final String TAG = CategoryFragment.class.getName();
 
-    public ProfileFragment() {
+    public CategoryFragment() {
         // Required empty public constructor
     }
 
@@ -29,13 +29,12 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        Log.d(TAG, "onCreateView: ");
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_category, container, false);
     }
 
     @Override
-    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
-        super.onViewStateRestored(savedInstanceState);
-
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
