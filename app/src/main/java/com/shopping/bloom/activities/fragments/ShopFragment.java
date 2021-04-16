a188379f6d112ead5aa86eb8e7e0bbc39ff41c25
vijay <36580862+hey-vijay@users.noreply.github.com>
2021-04-16 12:46:35 +0530

    Added UI for shop fragment

--- app/src/main/java/com/shopping/bloom/activities/fragments/ShopFragment.java
+++ app/src/main/java/com/shopping/bloom/activities/fragments/ShopFragment.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.shopping.bloom.R;
 
