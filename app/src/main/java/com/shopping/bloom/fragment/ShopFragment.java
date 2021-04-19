dd87529413aadfa7e0b18abceff929e622a5f29d
vijay <36580862+hey-vijay@users.noreply.github.com>
2021-04-19 11:42:14 +0530

    ViewPager indicator added

--- app/src/main/java/com/shopping/bloom/fragment/ShopFragment.java
+++ app/src/main/java/com/shopping/bloom/fragment/ShopFragment.java
@@ -1,11 +1,6 @@
 package com.shopping.bloom.fragment;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -13,13 +8,31 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.shopping.bloom.R;
+import com.shopping.bloom.adapters.ViewpagerAdapter;
 import com.shopping.bloom.firebaseConfig.RemoteConfig;
 import com.shopping.bloom.models.MainScreenImageModel;
+import com.shopping.bloom.restService.callback.ViewPagerClickListener;
+
+import java.util.List;
 
 public class ShopFragment extends Fragment {
+
     private static final String TAG = "ShopFragment";
+
+    private ViewPager2 vpHeaderImages;
+    List<MainScreenImageModel> mainScreenImageModel;
+    ViewpagerAdapter viewpagerAdapter;
+    LinearLayout vpIndicator;
 
     public ShopFragment() {
         // Required empty public constructor
@@ -36,9 +49,10 @@ public class ShopFragment extends Fragment {
                              Bundle savedInstanceState) {
         Log.d(TAG, "onCreateView: ");
 
-      MainScreenImageModel mainScreenImageModel  =  RemoteConfig.getMainScreenConfig(getContext()).getViewpager_image().get(0) ;
-        Log.d(TAG , mainScreenImageModel.getImagepath() + "\n " +  mainScreenImageModel.getId() + "\n " + mainScreenImageModel.getOrder() + "\n "  );
-        Log.d(TAG , RemoteConfig.getMainScreenConfig(getContext()).getSaleimagepath() );
+        mainScreenImageModel = RemoteConfig.getMainScreenConfig(getContext()).getViewpager_image();
+        Log.d(TAG, mainScreenImageModel.get(0).getImagepath() + "\n ");
+        Log.d(TAG, RemoteConfig.getMainScreenConfig(getContext()).getSaleimagepath());
+        Log.d(TAG, "onCreateView: mainScreenImageModel " + mainScreenImageModel.size());
 
         return inflater.inflate(R.layout.fragment_shop, container, false);
     }
@@ -47,6 +61,63 @@ public class ShopFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         getActivity().invalidateOptionsMenu();
+        initViews(view);
+        loadViewPager();
+
+    }
+
+    private void initViews(View view) {
+        vpHeaderImages = view.findViewById(R.id.vpHeaderImages);
+        vpIndicator = view.findViewById(R.id.vpIndicator);
+
+        viewpagerAdapter = new ViewpagerAdapter(mainScreenImageModel, getContext(), imageModel -> {
+            Log.d(TAG, "initViews: viewpager clicked " + imageModel.getImagepath() +
+                    " ID: "+ imageModel.getId());
+        });
+        setUpIndicator();
+    }
+
+    private void loadViewPager() {
+        if(!mainScreenImageModel.isEmpty()) {
+            vpHeaderImages.setAdapter(viewpagerAdapter);
+            viewpagerAdapter.notifyDataSetChanged();
+        }
+        vpHeaderImages.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                setCurrentIndicator(position);
+            }
+        });
+    }
+
+    private void setUpIndicator(){
+        ImageView[] indicator = new ImageView[viewpagerAdapter.getItemCount()];
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        params.setMargins(8,0,8,0);
+        for(int i = 0; i < indicator.length; i++){
+            indicator[i] = new ImageView(getContext().getApplicationContext());
+            indicator[i].setImageDrawable(ContextCompat.getDrawable(getContext().getApplicationContext(),
+                    R.drawable.vp_indicator_inactive));
+            indicator[i].setLayoutParams(params);
+            vpIndicator.addView(indicator[i]);
+        }
+    }
+
+    private void setCurrentIndicator(int position){
+        int childViewCount = vpIndicator.getChildCount();
+        for(int i = 0; i < childViewCount; i++){
+            ImageView imageView = (ImageView) vpIndicator.getChildAt(i);
+            if(i == position){
+                imageView.setImageDrawable(ContextCompat.getDrawable(getContext().getApplicationContext(),
+                        R.drawable.vp_indicator_active));
+            }else{
+                imageView.setImageDrawable(ContextCompat.getDrawable(getContext().getApplicationContext(),
+                        R.drawable.vp_indicator_inactive));
+            }
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/shopping/bloom/restService/callback/ViewPagerClickListener.java
@@ -0,0 +1,7 @@
+package com.shopping.bloom.restService.callback;
+
+import com.shopping.bloom.models.MainScreenImageModel;
+
+public interface ViewPagerClickListener {
+    void onClick(MainScreenImageModel imageModel);
+}
